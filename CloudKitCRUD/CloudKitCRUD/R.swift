06d268bb76b4f98df20765d382b44ef33b0249d8
steve thompson <steven.thompson.ca@gmail.com>
2018-07-26 16:48:54 -0400

    Adds R file

---
+++ CloudKitCRUD/CloudKitCRUD/R.swift
@@ -0,0 +1,18 @@
+//
+//  R.swift
+//  CloudKitCRUD
+//
+//  Created by steve on 2018-07-26.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+import Foundation
+
+enum R {
+  static let Person = "Person"
+  static let firstName = "firstName"
+  static let lastName = "lastName"
+  static let age = "age"
+  static let subscriptionID = "person-changes"
+  static let subscriptionWasCreated = "subscriptionWasCreated"
+}
