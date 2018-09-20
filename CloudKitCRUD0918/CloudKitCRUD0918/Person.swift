6f753de8c8d0e49a61de3cd604fd2c18abed04a8
steve thompson <steven.thompson.ca@gmail.com>
2018-09-20 09:09:39 -0400

    added Person

--- CloudKitCRUD0918/CloudKitCRUD0918/Person.swift
+++ CloudKitCRUD0918/CloudKitCRUD0918/Person.swift
@@ -24,15 +24,32 @@ final class Person {
   let lastName: String
   let age: Int
   var dogs: [CKRecord.Reference]? = nil
+  var record: CKRecord? = nil
   
-  init?(_ record: CKRecord) {
-    guard let fn = record[Key.firstName.rawValue] as? String, let ln = record[Key.lastName.rawValue] as? String, let a = record[Key.age.rawValue] as? Int else { return nil }
-    self.firstName = fn
-    self.lastName = ln
-    self.age = a
-    if let d = record[Key.dogs.rawValue] as? [CKRecord.Reference] {
-      self.dogs = d
+  init(firstName: String, lastName: String, age: Int, dogs: [CKRecord.Reference]? = nil, record: CKRecord? = nil) {
+    self.firstName = firstName
+    self.lastName = lastName
+    self.age = age
+    self.dogs = dogs
+    self.record = record
+    if self.record == nil {
+//      createRecord()
     }
+  }
+  
+//  private func createRecord() {
+//    self.record = CKRecord(recordType: Person.type)
+//    self.record[Key.firstName] = self.firstName
+//    self.record[Key.lastName]
+//  }
+//
+  convenience init?(_ record: CKRecord) {
+    guard let fn = record[Key.firstName.rawValue] as? String, let ln = record[Key.lastName.rawValue] as? String, let a = record[Key.age.rawValue] as? Int else { return nil }
+    var d: [CKRecord.Reference]? = nil
+    if let dogs = record[Key.dogs.rawValue] as? [CKRecord.Reference] {
+      d = dogs
+    }
+    self.init(firstName: fn, lastName: ln, age: a, dogs: d)
   }
   
 }
